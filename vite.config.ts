5d6504b09789571eeccdedfd39bb7356522d8947
mareddka <mareddka@mail.uc.edu>
2025-07-14 11:37:52 -0400

    Finalize config and deploy updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/spidr-airfryer-form/', 
+  base: '/spidr-airfryer-form/',
   plugins: [react()],
+  build: {
+    minify: false, 
+  },
 });
